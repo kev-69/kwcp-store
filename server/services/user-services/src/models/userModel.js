d919c9eb306afae7281acda26bd9f5661c477b14
kev-69 <bismarkobuobi19@gmail.com>
2025-04-06 16:51:40 +0000

    Add User model and address routes for user address management

---
+++ server/services/user-services/src/models/userModel.js
@@ -0,0 +1,42 @@
+const sequelize = require('../config/userdb.js');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+
+}, {
+    tableName: 'users',
+});
+
+module.exports = User;
