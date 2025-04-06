d3dd03fd5962c8afcdec2be620dfb98e2537ce05
kev-69 <bismarkobuobi19@gmail.com>
2025-04-06 15:11:47 +0000

    Add user routes for managing user operations

---
+++ server/services/user-services/src/routes/userRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const { 
+    getAllUsers, 
+    getUserById, 
+    createUser, 
+    updateUserById, 
+    deleteUserById, 
+    loginUser 
+} = require('../user-controllers/userController');
+
+// const authMiddleware = require('../middlewares/authMiddleware');
+
+router.get('/all', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/create', createUser);
+router.put('/update/:id', updateUserById);
+router.delete('/delete/:id', deleteUserById);
+
+router.post('/login', loginUser);
+
+module.exports = router;
