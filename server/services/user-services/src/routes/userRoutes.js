59bc756aabe205e64eefb30b59cfde7a823efdd5
kev-69 <bismarkobuobi19@gmail.com>
2025-04-07 11:03:50 +0000

    Refactor user routes: remove unused endpoints for user retrieval, creation, and login

--- server/services/user-services/src/routes/userRoutes.js
+++ server/services/user-services/src/routes/userRoutes.js
@@ -2,22 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    getAllUsers, 
     getUserById, 
-    createUser, 
     updateUserById, 
     deleteUserById, 
-    loginUser 
 } = require('../controllers/userController');
 
-// const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/all', getAllUsers);
 router.get('/:id', getUserById);
-router.post('/create', createUser);
 router.put('/update/:id', updateUserById);
 router.delete('/delete/:id', deleteUserById);
-
-router.post('/login', loginUser);
 
 module.exports = router;
