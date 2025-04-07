e2cb9b6bdc50aab263ac710c6bf73482ae8d2987
kev-69 <bismarkobuobi19@gmail.com>
2025-04-07 11:05:11 +0000

    Add database configuration and connection setup for Sequelize

---
+++ server/services/product-services/src/config/productdb.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.PG_DATABASE, 
+    process.env.PG_USER, 
+    process.env.PG_PASSWORD, {
+        host: process.env.PG_HOST,
+        dialect: 'postgres',
+        port: process.env.PG_PORT,
+    },
+)
+
+// Test the connection to the database
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.');
+    }) 
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+module.exports = sequelize;
