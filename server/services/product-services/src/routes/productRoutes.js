1fae9a79a8ade791df79d79c35d0de1689b169d4
kev-69 <bismarkobuobi19@gmail.com>
2025-04-08 08:21:56 +0000

    Add product routes for CRUD operations

---
+++ server/services/product-services/src/routes/productRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middlewares/upload');
+
+const {
+    addProduct,
+    getAllProducts,
+    getProductById,
+    updateProductById,
+    deleteProductById,
+} = require('../controllers/productController');
+
+// Routes for product operations
+router.post('/add', upload, addProduct);
+router.get('/all', getAllProducts);
+router.get('/:id', getProductById);
+router.put('/update/:id', updateProductById);
+router.delete('/delete/:id', deleteProductById);
+
+module.exports = router;
