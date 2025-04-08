2877751c0d167e87a0fcd2135b8420006734a319
kev-69 <bismarkobuobi19@gmail.com>
2025-04-08 08:17:29 +0000

    Enhance validation for User model fields and comment out timestamp fields

--- server/services/user-services/src/models/userModel.js
+++ server/services/user-services/src/models/userModel.js
@@ -10,35 +10,47 @@ const User = sequelize.define('User', {
     first_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
     },
     last_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+            isEmail: true,
+        },
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
     },
     role : {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: 'user',
     },
-    created_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-    },
+    // created_at: {
+    //     type: DataTypes.DATE,
+    //     allowNull: false,
+    //     defaultValue: DataTypes.NOW,
+    // },
+    // updated_at: {
+    //     type: DataTypes.DATE,
+    //     allowNull: false,
+    //     defaultValue: DataTypes.NOW,
+    // },
 
 }, {
     tableName: 'users',
