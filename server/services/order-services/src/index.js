32a5af6d2112725a1bcd5f6c0c89a5d05e7f5ceb
kev-69 <bismarkobuobi19@gmail.com>
2025-04-09 18:25:00 +0000

    Comment out model associations in order services for clarity

--- server/services/order-services/src/index.js
+++ server/services/order-services/src/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const sequelize = require('./configs/ordersdb')
-const Order = require('./models/orderModel')
-const Cart = require('./models/cartModel')
-const Products = require('../../product-services/src/models/productModel')
-const User = require('../../user-services/src/models/userModel')
+// const Order = require('./models/orderModel')
+// const Cart = require('./models/cartModel')
+// const Products = require('../../product-services/src/models/productModel')
+// const User = require('../../user-services/src/models/userModel')
 
 // call routes
 const orderRoutes = require('./routes/orderRoutes')
@@ -14,7 +14,7 @@ const cartRoutes = require('./routes/cartRoutes')
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3007
 
 // sync database
 sequelize.sync({ force: false }) // Set to true only for development to drop and recreate tables
@@ -35,17 +35,17 @@ app.use("/order", orderRoutes)
 app.use("/cart", cartRoutes)
 
 // Define associations
-Order.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-User.hasMany(Order, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Order.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// User.hasMany(Order, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-Order.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-Products.hasMany(Order, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Order.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Products.hasMany(Order, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-Cart.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-User.hasMany(Cart, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Cart.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// User.hasMany(Cart, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-Cart.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-Products.hasMany(Cart, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Cart.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+// Products.hasMany(Cart, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
 
 app.listen(PORT, () => {
