f88e89a5e0ec6b58283f4cba8558a4c9def894e8
kev-69 <bismarkobuobi19@gmail.com>
2025-04-09 17:33:20 +0000

    Add cart model with associations for user and product

---
+++ server/services/order-services/src/models/cartModel.js
@@ -0,0 +1,48 @@
+const sequelize = require('../configs/ordersdb')
+const { DataTypes } = require('sequelize')
+
+const Products = require('../../../product-services/src/models/productModel')
+const User = require('../../../user-services/src/models/userModel')
+
+const Cart = sequelize.define('Cart', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        },
+        onDelete: 'CASCADE', // When a user is deleted, all orders for that user will also be deleted
+        onUpdate: 'CASCADE', // When a user is updated, the corresponding order's user_id will also be updated
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Products,
+            key: 'id',
+        },
+        onDelete: 'CASCADE', // When a product is deleted, all orders for that product will also be deleted
+        onUpdate: 'CASCADE', // When a product is updated, the corresponding order's product_id will also be updated
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+}, {
+    tableName: 'cart'
+})
+
+// Define associations
+Cart.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+User.hasMany(Cart, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+
+Cart.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+Products.hasMany(Cart, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+
+module.exports = Cart
