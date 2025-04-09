1551961345b61c49ff4924ec8ec7a698e72ec007
kev-69 <bismarkobuobi19@gmail.com>
2025-04-09 18:22:35 +0000

    Remove Order associations from Product model in product services

--- server/services/product-services/src/index.js
+++ server/services/product-services/src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const sequelize = require('./config/productdb');
 const Product = require('./models/productModel')
 const Category = require('./models/categoryModel')
-const Orders = require('../../order-services/src/models/orderModel')
+
 
 const productRoutes = require('./routes/productRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -40,9 +40,6 @@ app.use("/category", categoryRoutes)
 // Define associations
 Product.belongsTo(Category, { foreignKey: 'category_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 Category.hasMany(Product, { foreignKey: 'category_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-
-Product.hasMany(Orders, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-Orders.belongsTo(Product, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
 app.listen(PORT, () => {
     console.log(`Products server is running on http://localhost:${PORT}`);
