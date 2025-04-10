589a122722ab831cdfb8944251e3fcd01926f7fe
kev-69 <bismarkobuobi19@gmail.com>
2025-04-10 06:33:58 +0000

    Comment out associations in Cart model for clarity

--- server/services/order-services/src/models/cartModel.js
+++ server/services/order-services/src/models/cartModel.js
@@ -10,22 +10,10 @@ const Cart = sequelize.define('Cart', {
     user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        references: {
-            model: "users",
-            key: 'id',
-        },
-        onDelete: 'CASCADE', // When a user is deleted, all orders for that user will also be deleted
-        onUpdate: 'CASCADE', // When a user is updated, the corresponding order's user_id will also be updated
     },
     product_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        references: {
-            model: "products",
-            key: 'id',
-        },
-        onDelete: 'CASCADE', // When a product is deleted, all orders for that product will also be deleted
-        onUpdate: 'CASCADE', // When a product is updated, the corresponding order's product_id will also be updated
     },
     quantity: {
         type: DataTypes.INTEGER,
@@ -34,12 +22,5 @@ const Cart = sequelize.define('Cart', {
 }, {
     tableName: 'cart'
 })
-
-// Define associations
-// Cart.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-// User.hasMany(Cart, { foreignKey: 'user_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-
-// Cart.belongsTo(Products, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-// Products.hasMany(Cart, { foreignKey: 'product_id', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
 module.exports = Cart
