85e97ad7cd30f67f49196a18bf6c4a468896d346
kev-69 <bismarkobuobi19@gmail.com>
2025-04-11 09:57:37 +0000

    Implement cart functionality with Redis: addToCart, viewCart, and checkout methods

--- server/services/order-services/src/controllers/orderController.js
+++ server/services/order-services/src/controllers/orderController.js
@@ -1,6 +1,68 @@
 const OrderServices = require('../services/orderServices');
+const redisClient = require('../configs/redis'); // Redis client setup
 
 const { check, validationResult } = require('express-validator');
+
+
+const addToCart = async (req, res) => {
+    try {
+        const { user_id, product_id, quantity, price } = req.body
+        if (!user_id || !product_id || !quantity || !price) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const cartKey = `cart:${user_id}`;
+        const itemKey = `${product_id}`;
+        const itemData = { product_id, quantity, price };
+
+        // Add item to redis cart
+        await redisClient.hSet(cartKey, itemKey, JSON.stringify(itemData))
+        return res.status(200).json({ message: "Items added to cart", itemData })
+    } catch (error) {
+        return res.status(500).json({ error: error})
+    }
+};
+
+const viewCart = async (req, res) => {
+    try {
+        const { user_id } = req.body
+        const cartKey = `cart:${user_id}`
+
+        // retrieve cart items from redis
+        const cartItems = await redisClient.hGetAll(cartKey)
+        const items = Object.values(cartItems).map(item => JSON.parse(item))
+
+        return res.json(items)
+    } catch (error) {
+        return res.status(500).json({ error: error })
+    }
+}
+
+const checkout = async (req, res) => {
+    try {
+        const { user_id, total_amount, status } = req.body
+        const cartKey = `cart:${user_id}`
+
+        // retrieve cart items from redis
+        const cartItems = await redisClient.hGetAll(cartKey)
+        const items = Object.values(cartItems).map(item => JSON.parse(item))
+
+        // check
+        if (!items || items.length === 0) {
+            return res.status(400).json({ message: "Cart is empty" })
+        }
+
+        // save order to database
+        const order = await OrderServices.createOrder({ user_id, total_amount, status, items })
+
+        // clear cart after checkout
+        await redisClient.del(cartKey);
+
+        return res.status(201).json({ message: "Order successfully checked out", order })
+    } catch (error) {
+        return res.status(500).json({ error: error })
+    }
+}
 
 const createOrder = async (req, res) => {
     try {
@@ -61,6 +123,9 @@ const getAllOrders = async (req, res) => {
 };
 
 module.exports = { 
+    addToCart,
+    viewCart,
+    checkout,
     createOrder, 
     getOrderById, 
     updateOrderById, 
